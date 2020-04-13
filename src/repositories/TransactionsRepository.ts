6fa3b4cc1fa70e503201eafd2a2c7cbd84374b4c
Wiliam Omori <wiliam.omori@gmail.com>
2020-04-13 18:16:41 -0300

    Desafio 05: Primeiro projeto Node.js

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,6 +1,12 @@
 import Transaction from '../models/Transaction';
 
-interface Balance {
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface BalanceDTO {
   income: number;
   outcome: number;
   total: number;
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
-  public getBalance(): Balance {
-    // TODO
+  public getBalance(): BalanceDTO {
+    const income = this.transactions.reduce(
+      (total: number, { type, value }: Transaction) => {
+        if (type === 'income') return total + value;
+        return total;
+      },
+      0,
+    );
+
+    const outcome = this.transactions.reduce(
+      (total: number, { type, value }: Transaction) => {
+        if (type === 'outcome') return total + value;
+        return total;
+      },
+      0,
+    );
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,18 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: TransactionDTO): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) throw Error('Limit exceeded');
+
+    const transation = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transation;
   }
 }
 
