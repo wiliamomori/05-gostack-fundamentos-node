ac66e3a8011f2ce35f8f8a46b3dfb52a39b5db76
Wiliam Omori <wiliam.omori@gmail.com>
2020-04-14 13:40:14 -0300

    add validate type transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -17,6 +17,9 @@ class CreateTransactionService {
   public execute({ title, value, type }: TransactionDTO): Transaction {
     const { total } = this.transactionsRepository.getBalance();
 
+    if (!['income', 'outcome'].includes(type))
+      throw new Error('Transaction type is not valid');
+
     if (type === 'outcome' && value > total) throw Error('Limit exceeded');
 
     const transation = this.transactionsRepository.create({
